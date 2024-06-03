4fd96ea5ab98be917322b19b678a5363c341dc0d
Tetiana Tolmachova <totatiana2015@gmail.com>
2024-06-03 15:39:01 +0200

    hw-3

---
+++ js/task-2.js
@@ -0,0 +1,12 @@
+"use strict";
+function makeArray(firstArray, secondArray, maxLength) {
+  let union = firstArray.concat(secondArray);
+  return union.length > maxLength ? union.slice(0, maxLength) : union;
+}
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
